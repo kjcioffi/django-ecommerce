f80511a5fd334612b15b36223682161347e030a4
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-01-31 13:43:39 -0500

    Added models for ecommerce store.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     'debug_toolbar',
+    'store',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/', height_field=300, width_field=300)
+
+
+class Order(models.Model):
+    product = models.ManyToManyField(Product)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = PhoneNumberField()
+    street = models.CharField(max_length=50)
+    zip = models.CharField(max_length=5)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
