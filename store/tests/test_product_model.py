cf31841550e0ebbfc955b3588b59138e262ccd9a
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-01-31 18:35:51 -0500

    Unit test for testing name attribute and accepting 100 chars.

--- store/tests/test_product_model.py
+++ store/tests/test_product_model.py
@@ -1,17 +1,31 @@
 import random
 import string
+from decimal import Decimal
 from django.forms import ValidationError
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from store.models import Product
 
 
 class ProductModel(TestCase):
-    def test_name_rejects_over_100_chars(self):
-        letters = string.ascii_lowercase
-        product = Product(name=''.join(random.choice(letters) for _ in range(101)))
-        
-        with self.assertRaises(ValidationError) as full_clean:
-            product.full_clean()
+    def setUp(self):
+        self.letters = string.ascii_lowercase
+        self.product = Product(name=''.join(random.choice(self.letters) for _ in range(100)),
+                               rating=3,
+                               price=Decimal('10.99'),
+                               description="Test")
 
-        self.assertIn('name', full_clean.exception.message_dict)
+    def test_name_rejects_over_100_chars(self):
+        self.product.name += '*'
+        
+        with self.assertRaises(ValidationError) as e:
+            self.product.full_clean()
+
+        self.assertIn('name', e.exception.message_dict)
+
+    def test_name_accepts_100_chars(self):
+        try:
+            self.product.full_clean()
+        except ValidationError as e:
+            self.assertNotIn('name', e.message_dict)
