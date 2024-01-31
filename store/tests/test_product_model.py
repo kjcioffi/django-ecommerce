b31e068d7fa773f1062693f799889395fb5254f4
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-01-31 14:42:07 -0500

    First unit-test for product model.

---
+++ store/tests/test_product_model.py
@@ -0,0 +1,15 @@
+import random
+import string
+from django.forms import ValidationError
+from django.test import TestCase
+
+from store.models import Product
+
+
+class ProductModel(TestCase):
+    def test_name_rejects_over_100_chars(self):
+        letters = string.ascii_lowercase
+        product = Product(name=''.join(random.choice(letters) for _ in range(101)))
+        
+        with self.assertRaises(ValidationError):
+            product.full_clean()
