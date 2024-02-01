6c3554e49352562e39fc3a1170b386db5a361935
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-01 11:18:50 -0500

    Added products to the project.

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ["name", "rating", "price", "description", "image"]
