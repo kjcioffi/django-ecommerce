4afbc32ff128ffab2fda63793e0d82e6ebc80de1
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-01 09:16:02 -0500

    Refactored to reduce redundancy on tests for rating attribute.

--- store/tests/test_product_model.py
+++ store/tests/test_product_model.py
@@ -10,34 +10,32 @@ from store.models import Product
 class ProductModel(TestCase):
     def setUp(self):
         self.letters = string.ascii_lowercase
-        self.product = Product(name=''.join(random.choice(self.letters) for _ in range(100)),
+        self.product_name = ''.join(random.choice(self.letters) for _ in range(100))
+        self.product = Product(name=self.product_name,
                                rating=0,
                                price=Decimal('10.99'),
                                description="Test")
 
+    def validate_rating(self, rating: int, should_pass: bool):
+        self.product.rating = rating
+        if should_pass:
+            try:
+                self.product.full_clean()
+            except ValidationError as e:
+                self.assertNotIn('rating', e.message_dict)
+        else:
+            with self.assertRaises(ValidationError) as e:
+                self.product.full_clean()
+            self.assertIn('rating', e.exception.message_dict)
+
     def test_rating_below_minimum(self):
-        self.product.rating = -1
-        with self.assertRaises(ValidationError) as e:
-            self.product.full_clean()
-        
-        self.assertIn('rating', e.exception.message_dict)
+        self.validate_rating(-1, False)
 
     def test_rating_above_maximum(self):
-        self.product.rating = 11
-        with self.assertRaises(ValidationError) as e:
-            self.product.full_clean()
-        
-        self.assertIn('rating', e.exception.message_dict)
+        self.validate_rating(11, False)
 
     def test_rating_at_minimum(self):
-        try:
-            self.product.full_clean()
-        except ValidationError as e:
-            self.assertNotIn('rating', e.message_dict)
+        self.validate_rating(0, True)
 
     def test_rating_at_maximum(self):
-        self.product.rating = 10
-        try:
-            self.product.full_clean()
-        except ValidationError as e:
-            self.assertNotIn('rating', e.message_dict)
+        self.validate_rating(10, True)
