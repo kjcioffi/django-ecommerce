ddec5e745912ba805dd651020c783c855c25c868
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-01 08:59:50 -0500

    Added boundary condition tests for rating attribute on product model.

--- store/tests/test_product_model.py
+++ store/tests/test_product_model.py
@@ -11,6 +11,33 @@ class ProductModel(TestCase):
     def setUp(self):
         self.letters = string.ascii_lowercase
         self.product = Product(name=''.join(random.choice(self.letters) for _ in range(100)),
-                               rating=3,
+                               rating=0,
                                price=Decimal('10.99'),
                                description="Test")
+
+    def test_rating_below_minimum(self):
+        self.product.rating = -1
+        with self.assertRaises(ValidationError) as e:
+            self.product.full_clean()
+        
+        self.assertIn('rating', e.exception.message_dict)
+
+    def test_rating_above_maximum(self):
+        self.product.rating = 11
+        with self.assertRaises(ValidationError) as e:
+            self.product.full_clean()
+        
+        self.assertIn('rating', e.exception.message_dict)
+
+    def test_rating_at_minimum(self):
+        try:
+            self.product.full_clean()
+        except ValidationError as e:
+            self.assertNotIn('rating', e.message_dict)
+
+    def test_rating_at_maximum(self):
+        self.product.rating = 10
+        try:
+            self.product.full_clean()
+        except ValidationError as e:
+            self.assertNotIn('rating', e.message_dict)
