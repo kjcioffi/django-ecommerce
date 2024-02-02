c582f794249f7e1344b7561502cb7ce4c5186ace
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-02 11:56:51 -0500

    Draft design created for Index ListView.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from ecommerce import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('store.urls', 'store'), namespace='store')),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
