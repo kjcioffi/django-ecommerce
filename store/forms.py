424c86fa20dac0b2c915915263b8c6570933747d
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-08 14:22:28 -0500

    Return proper message for invalid phone number in order form.

--- store/forms.py
+++ store/forms.py
@@ -2,7 +2,18 @@ from django import forms
 
 from store.models import Order
 
+import re
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         exclude = ["products"]
+        
+
+    def clean_phone_number(self):
+        pattern = re.compile(r'^\d{3}-\d{3}-\d{4}$')
+        phone_number: str = self.cleaned_data.get('phone_number')
+
+        if not pattern.match(phone_number):
+            raise forms.ValidationError('Please use XXX-XXX-XXXX format.')
+        return phone_number
--- store/tests/test_order_form.py
+++ store/tests/test_order_form.py
@@ -18,3 +18,8 @@ class TestOrderForm(TestCase):
     def test_order_form_valid(self):
         form = OrderForm(self.form_data)
         self.assertTrue(form.is_valid())
+    
+    def test_phone_number_field_invalid(self):
+        self.form_data['phone_number'] = '(555) 555-5555'
+        form = OrderForm(self.form_data)
+        self.assertFormError(form, 'phone_number', 'Please use XXX-XXX-XXXX format.')
