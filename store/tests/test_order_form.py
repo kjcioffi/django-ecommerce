b55a645aa74d0336a854157da3cb00641a288eee
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-09 08:59:55 -0500

    Test for associating products with orders.

--- store/tests/test_order_form.py
+++ store/tests/test_order_form.py
@@ -1,12 +1,15 @@
-import pdb
-from django.forms import ValidationError
-from django.test import TestCase
+from decimal import Decimal
+import random as rand
+
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from store.forms import OrderForm
-from store.models import Order
+from store.models import Order, Product
 
 class TestOrderForm(TestCase):
     def setUp(self):
+        self.client = Client()
         self.form_data = {'first_name': 'Kevin', 'last_name': 'C', 'email': 'kc@example.com',
                                   'phone_number': '555-555-5555', 'street': 'Test St.', 'zip': '12345',
                                   'city': 'Test City', 'state': 'Testimonial'}
@@ -30,3 +33,43 @@ class TestOrderForm(TestCase):
         self.assertTrue(form.is_valid(), 'The form should be valid.')
         model_object = form.save()
         self.assertTrue(Order.objects.filter(pk=model_object.pk).exists(), 'The order object should exist in the database after saving.')
+
+    def test_products_associated_with_order(self):
+        """
+        Test that products added to a simulated bag are correctly associated with an order.    
+        """
+        for _ in range(0, 10):
+            self.client.post(reverse('store:add-to-bag'), {'product_id': rand.randint(11, 19)})
+
+        bag: list = self.client.session.get('bag', [])
+
+        order = Order.objects.create(
+            first_name='John',
+            last_name='Doe',
+            email='john@example.com',
+            phone_number='555-555-5555',
+            street='123 Main St',
+            zip='12345',
+            city='Cityville',
+            state='State'
+        )
+
+        self.create_products(10)
+
+        for item in bag:
+            product = Product.objects.get(id=int(item['product_id']))
+            order.products.add(product)
+
+        expected_product_ids: list = [item['product_id'] for item in bag]
+        actual_product_ids: list = [product.id for product in order.products.all()]
+
+        self.assertTrue(all(product_id in expected_product_ids for product_id in actual_product_ids))
+        self.assertEqual(len(bag), order.products.count())
+
+
+    def create_products(self, number: int):
+        for i in range(1, number):
+            Product.objects.create(name=f"Product {i}",
+                                rating=0,
+                                price=Decimal('10.99'),
+                                description="Test")
