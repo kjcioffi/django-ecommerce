7811eb592fa06ee110b503f491d1d35817c79e9b
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-10 17:27:06 -0500

    Form field errors populate in checkout view when validation fails.

--- store/tests/test_checkout_view.py
+++ store/tests/test_checkout_view.py
@@ -27,4 +27,21 @@ class TestCheckoutView(TestCase):
         self.assertEqual(response.status_code, 302)
         saved_order = Order.objects.filter(first_name="Michael", last_name="Dillon")
         self.assertTrue(saved_order.exists())
+
+    def test_form_submit_fails_with_field_errors(self):
+        bad_form_data = {"first_name": "",
+                            "last_name": "",
+                            "email": "michael@hotmail.com",
+                            "phone_number": "(875) 844-6600",
+                            "street": "7762 Newman Flats Suite 388",
+                            "zip": "24306",
+                            "city": "New Cindychester",
+                            "state": "Pennsylvania"}
+        
+        response = self.client.post(reverse('store:checkout'), bad_form_data)
+        form = response.context['form']
+
+        self.assertFormError(form, 'first_name', 'This field is required.')
+        self.assertFormError(form, 'last_name', 'This field is required.')
+        self.assertFormError(form, 'phone_number', 'Please use XXX-XXX-XXXX format.')
         
