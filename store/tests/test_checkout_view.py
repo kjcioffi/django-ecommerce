6f1360a1b4a9cf25cb67243e6ed4d5942f2d0ec6
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-02-14 13:43:54 -0500

    Refactored use of request/response cycle in test_checkout_view.py.

--- store/tests/test_checkout_view.py
+++ store/tests/test_checkout_view.py
@@ -24,16 +24,17 @@ class TestCheckoutView(TestCase):
                             "zip": "24306",
                             "city": "New Cindychester",
                             "state": "Pennsylvania"}
+        
+        self.response = self.client.get(reverse('store:checkout'))
 
     def test_form_display(self):
-        response = self.client.get(reverse('store:checkout'))
-        self.assertEqual(response.status_code, 200)
-        form = response.context['form']
+        self.assertEqual(self.response.status_code, 200)
+        form = self.response.context['form']
         self.assertTrue(form)
 
     def test_form_submit_success(self):
-        response = self.client.post(reverse('store:checkout'), self.form_data)
-        self.assertEqual(response.status_code, 302)
+        request = self.post_form_data(self.form_data)
+        self.assertEqual(request.status_code, 302)
         saved_order = Order.objects.filter(first_name="Michael", last_name="Dillon")
         self.assertTrue(saved_order.exists())
 
@@ -47,8 +48,8 @@ class TestCheckoutView(TestCase):
                             "city": "",
                             "state": ""}
         
-        response = self.client.post(reverse('store:checkout'), bad_form_data)
-        form = response.context['form']
+        request = self.post_form_data(bad_form_data)
+        form = request.context['form']
 
         self.assertFormError(form, 'first_name', 'This field is required.')
         self.assertFormError(form, 'last_name', 'This field is required.')
@@ -63,12 +64,11 @@ class TestCheckoutView(TestCase):
             self.assertFormError(form, 'phone_number', 'Please use XXX-XXX-XXXX format.')
 
     def test_quantities_in_context_match_up_with_session(self):
-        response = self.client.get(reverse('store:checkout'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
         session_bag_product_quantities: dict = {item['product_id']: item['quantity'] for item in self.session['bag']}
 
-        for product_in_context in response.context['products_in_bag']:
+        for product_in_context in self.response.context['products_in_bag']:
             product_id = product_in_context['product'].id
             product_quantity = product_in_context['quantity']
 
@@ -79,26 +79,26 @@ class TestCheckoutView(TestCase):
         self.session['bag'] = []
         self.session.save()
 
-        response = self.client.get(reverse('store:checkout'))
+        response_with_updated_session = self.client.get(reverse('store:checkout'))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['products_in_bag'], [])
+        self.assertEqual(response_with_updated_session.status_code, 200)
+        self.assertEqual(response_with_updated_session.context['products_in_bag'], [])
 
     def test_session_total_equals_context_total(self):
-        response = self.client.get(reverse('store:checkout'))
-
         # products_in_bag context item maps the necessary session data and product model objects needed.
-        session_total = sum(bag_item['product'].price * bag_item['quantity'] for bag_item in response.context['products_in_bag'])
+        session_total = sum(bag_item['product'].price * bag_item['quantity'] for bag_item in self.response.context['products_in_bag'])
 
-        self.assertEqual(session_total, response.context['total_cost'])
+        self.assertEqual(session_total, self.response.context['total_cost'])
 
     def test_order_total_persists_after_order_placed(self):
-        response = self.client.get(reverse('store:checkout')) # get context items
-        request = self.client.post(reverse('store:checkout'), self.form_data)
+        request = self.post_form_data(self.form_data)
         self.assertEqual(request.status_code, 302)
 
         saved_order = Order.objects.filter(first_name="Michael", last_name="Dillon")
         self.assertTrue(saved_order.exists())
         saved_order = saved_order.get()
 
-        self.assertEqual(saved_order.total_cost, response.context["total_cost"])
+        self.assertEqual(saved_order.total_cost, self.response.context["total_cost"])
+
+    def post_form_data(self, form_data):
+        return self.client.post(reverse('store:checkout'), form_data)
