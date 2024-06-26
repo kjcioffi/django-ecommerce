1c1893782283e47c7e2774abd890cba09abcd1ed
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-06-26 12:58:39 -0400

    Switched development DB to SQLite3.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,7 +87,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db()
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
