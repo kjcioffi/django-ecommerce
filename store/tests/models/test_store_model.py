e710eeed1ddc7a3c862b6091325d666837062e66
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-02 13:11:51 -0400

    Linting and re-formatting.

--- store/tests/models/test_store_model.py
+++ store/tests/models/test_store_model.py
@@ -19,16 +19,27 @@ class TestStoreModel(TestCase):
         self.store: Store = StoreFactory()
 
     def test_name_within_100_chars(self):
-        self.assertLessEqual(len(self.store.name), self.name_char_length, f"Store name field must be {self.name_char_length} characters or less.")
+        self.assertLessEqual(
+            len(self.store.name),
+            self.name_char_length,
+            f"Store name field must be {self.name_char_length} characters or less.",
+        )
 
     def test_name_cant_exceed_100_chars(self):
-        name: str = ''.join(random.choice(string.ascii_letters) for _ in range(self.name_char_length + 1))
+        name: str = "".join(
+            random.choice(string.ascii_letters)
+            for _ in range(self.name_char_length + 1)
+        )
         self.store.name = name
 
         with self.assertRaises(ValidationError):
             self.store.clean_fields()
-            
-        self.assertEqual(len(self.store.name), self.name_char_length + 1, f"Store name field must be {self.name_char_length} characters or less.")
+
+        self.assertEqual(
+            len(self.store.name),
+            self.name_char_length + 1,
+            f"Store name field must be {self.name_char_length} characters or less.",
+        )
 
     def test_user_delete_cascades_store(self):
         self.store.owner.delete()
@@ -39,25 +50,47 @@ class TestStoreModel(TestCase):
         self.assertTrue(User.objects.filter(pk=self.store.owner.pk).exists())
 
     def test_city_within_25_chars(self):
-        self.assertLessEqual(len(self.store.city), self.city_char_length, f"Store name field must be {self.city_char_length} characters or less.")
+        self.assertLessEqual(
+            len(self.store.city),
+            self.city_char_length,
+            f"Store name field must be {self.city_char_length} characters or less.",
+        )
 
     def test_city_cant_exceed_25_chars(self):
-        city: str = ''.join(random.choice(string.ascii_letters) for _ in range(self.city_char_length + 1))
+        city: str = "".join(
+            random.choice(string.ascii_letters)
+            for _ in range(self.city_char_length + 1)
+        )
         self.store.city = city
 
         with self.assertRaises(ValidationError):
             self.store.clean_fields()
-            
-        self.assertEqual(len(self.store.city), self.city_char_length + 1, f"Store name field must be {self.city_char_length} characters or less.")
+
+        self.assertEqual(
+            len(self.store.city),
+            self.city_char_length + 1,
+            f"Store name field must be {self.city_char_length} characters or less.",
+        )
 
     def test_state_within_25_chars(self):
-        self.assertLessEqual(len(self.store.state), self.state_char_length, f"Store name field must be {self.state_char_length} characters or less.")
+        self.assertLessEqual(
+            len(self.store.state),
+            self.state_char_length,
+            f"Store name field must be {self.state_char_length} characters or less.",
+        )
 
     def test_state_cant_exceed_25_chars(self):
-        state: str = ''.join(random.choice(string.ascii_letters) for _ in range(self.state_char_length + 1))
+        state: str = "".join(
+            random.choice(string.ascii_letters)
+            for _ in range(self.state_char_length + 1)
+        )
         self.store.state = state
 
         with self.assertRaises(ValidationError):
             self.store.clean_fields()
-            
-        self.assertEqual(len(self.store.state), self.state_char_length + 1, f"Store name field must be {self.state_char_length} characters or less.")
+
+        self.assertEqual(
+            len(self.store.state),
+            self.state_char_length + 1,
+            f"Store name field must be {self.state_char_length} characters or less.",
+        )
