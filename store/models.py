792154906b73852663ac78a2f7220a5b39cc303f
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-03 13:43:48 -0400

    Implemented the store model into products.     - This represents another step toward making this a SaaS product.     - Added the associated migration file.

--- store/models.py
+++ store/models.py
@@ -14,11 +14,14 @@ class Store(models.Model):
 
 
 class Product(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+    )
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to="products/")
 
     def __str__(self):
         return f"{self.name}"
