3498af6909666e8bd2ac6b3a92da32a02369b7f6
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-03 13:41:37 -0400

    Refactored product model test suite to accomodate the new store model.    - Implemented a product factory that'll eventually replace the utility module in tests.    - Added some cascading edge cases to ensure a deletion anomaly doesn't occur.

---
+++ store/tests/factories/product_factory.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+import random
+import factory
+import faker_commerce
+from faker import Faker
+from store.models import Product
+from django.core.files.uploadedfile import SimpleUploadedFile
+from store.tests.factories.store_factory import StoreFactory
+
+
+faker: Faker = Faker()
+faker.add_provider(faker_commerce.Provider)
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    name = factory.Sequence(lambda _: faker.ecommerce_name())
+    rating = factory.Faker("pyint")
+    price = Decimal(random.randint(1, 1000))
+    description = factory.Faker("text")
+    image = SimpleUploadedFile(
+        name="test_image.jpeg",
+        content=open("media/tests/test_file.png", "rb").read(),
+        content_type="image/jpeg",
+    )
+
+    store = factory.SubFactory(StoreFactory)
