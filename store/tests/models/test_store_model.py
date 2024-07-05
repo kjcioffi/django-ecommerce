dc96995b7d66c9d8bc685588c5b29244470b5398
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-05 13:43:11 -0400

    Implemented unit-test checking Store state field cannot be empty.

--- store/tests/models/test_store_model.py
+++ store/tests/models/test_store_model.py
@@ -129,4 +129,10 @@ class TestStoreModel(TestCase):
             len(self.store.state),
             self.state_char_length + 1,
             f"Store state field must be {self.state_char_length} characters or less.",
-        )
+    
+    def test_state_cannot_be_empty(self):
+        with self.assertRaisesMessage(ValidationError, "") as e:
+            self.store.state = ""
+            self.store.clean_fields()
+
+        self.assertIn("state", e.exception.message_dict)
