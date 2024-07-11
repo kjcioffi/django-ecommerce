5058381fd6f02d7b00c0261c3be46cecd3d22f24
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-11 13:14:21 -0400

    Add ProductAdmin view to store/urls.py.

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import Index, ProductDetail, ProductAdmin, checkout, add_to_bag
 
 app_name = 'store'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetail.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
     path('add-to-bag/', add_to_bag, name='add-to-bag'),
+    path('user-admin/products', ProductAdmin.as_view(), name='product_admin'),
 ]
