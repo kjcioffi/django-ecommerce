661695a1542d9a25c38d84712b75a18659943028
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-15 12:45:14 -0400

    Change URL path route for product admin.

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,5 @@ from .views import StoreList, StoreProducts, ProductDetail, ProductAdmin, checko
 
 app_name = 'store'
 urlpatterns = [
-    path('store/<int:store_id>', StoreProducts.as_view(), name='store_front'),
-    path('', StoreList.as_view(), name='store_list'),
-    path('product/<int:pk>', ProductDetail.as_view(), name='product'),
-    path('checkout/', checkout, name='checkout'),
-    path('add-to-bag/', add_to_bag, name='add-to-bag'),
-    path('user-admin/products', ProductAdmin.as_view(), name='product_admin'),
+    path("user-admin/store/products", ProductAdmin.as_view(), name="product_admin"),
 ]
