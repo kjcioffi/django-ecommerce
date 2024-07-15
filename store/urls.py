6f571e2f6a93cd91f0fc39a344193212708d2346
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-15 12:47:09 -0400

    Add URL paths from main merge.

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import StoreList, StoreProducts, ProductDetail, ProductAdmin, checkout, add_to_bag
+from .views import (
+    StoreList,
+    StoreProducts,
+    ProductDetail,
+    ProductAdmin,
+    checkout,
+    add_to_bag,
+)
 
 app_name = 'store'
 urlpatterns = [
+    path("store/<int:store_id>", StoreProducts.as_view(), name="store_front"),
+    path("", StoreList.as_view(), name="store_list"),
+    path("store/product/<int:pk>", ProductDetail.as_view(), name="product"),
+    path("checkout/", checkout, name="checkout"),
+    path("add-to-bag/", add_to_bag, name="add-to-bag"),
     path("user-admin/store/products", ProductAdmin.as_view(), name="product_admin"),
 ]
