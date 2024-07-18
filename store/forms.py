22ecfbd14eb4a8cc924619437f405164541a2cca
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-07-18 12:29:19 -0400

    Exclude newly added store field. New logic is being incorporated into the checkout view.    - Removed phone number validation logic as it is going to be optional.

--- store/forms.py
+++ store/forms.py
@@ -7,17 +7,9 @@ import re
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        exclude = ["products", "total_cost"]
-        
+        exclude = ["store", "products", "total_cost"]
 
-    def clean_phone_number(self):
-        pattern = re.compile(r'^\d{3}-\d{3}-\d{4}$')
-        phone_number: str = self.cleaned_data.get('phone_number')
 
-        if not pattern.match(phone_number):
-            raise forms.ValidationError('Please use XXX-XXX-XXXX format.')
-        return phone_number
-    
 class ProductAdminForm(forms.ModelForm):
     class Meta:
         model = Product
